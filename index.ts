rafal-majewski/template-of-node-js-library

feature!: Kick-off the library

index.ts
@@ -1,0 +1,1 @@
+export type * as templateOfNodeJsLibrary from "./source/index.ts";

native-typescript/api-of-spotgo

index.ts
@@ -1,0 +1,1 @@
+export type * as clientOfApiOfSpotgo from "./source/index.ts";